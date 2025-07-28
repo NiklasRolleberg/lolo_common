9781302d231533c59ce51f1df26b63ef9f3b34d9
Niklas <nrol@kth.se>
2025-07-28 15:29:18 +0200

    Updated lolo geofence checker node

--- lolo_drivers/lolo_drivers/geofence_checker_client.py
+++ lolo_drivers/lolo_drivers/geofence_checker_client.py
@@ -5,6 +5,9 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
 
 import time
 from std_msgs.msg import Bool
@@ -20,8 +23,12 @@ class GeoFenceCheckerClient(Node):
         self.declare_parameter("verbose", False)
         self.verbose = self.get_parameter("verbose").value
 
+        srv_callback_group = ReentrantCallbackGroup()
+        other_callback_group = ReentrantCallbackGroup()
+        timer_callback_group = MutuallyExclusiveCallbackGroup()
+
         self.cli = self.create_client(srv_type=GeoFenceChecker,
-                                      srv_name=MissionTopics.GEOFENCE_CHECKER_SERVICE)
+                                      srv_name=MissionTopics.GEOFENCE_CHECKER_SERVICE, callback_group=srv_callback_group)
 
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for service geofence_checker...')
@@ -34,40 +41,42 @@ class GeoFenceCheckerClient(Node):
             GeoPoint,
             SmarcTopics.POS_LATLON_TOPIC,
             self.latlon_callback,
-            1)
+            1,
+            callback_group=timer_callback_group)
         
-        self.publisher = self.create_publisher(Bool, 'inside_geofence', 10)
+        self.publisher = self.create_publisher(Bool, 'inside_geofence', 10, callback_group=other_callback_group)
         self.robot_position = GeoPoint()
+
+        self.timer = self.create_timer(1, self.timer_callback, callback_group=timer_callback_group)
 
 
     def latlon_callback(self, msg):
+        #self.get_logger().info('\tlatlon callback')
         self.robot_position = msg
         self.req.geopoint = self.robot_position
         if(self.future is None):
-            #do service call
             self.future = self.cli.call_async(self.req)
 
-
-    def spin(self):
-        while rclpy.ok():
-            time.sleep(0.01)
-            rclpy.spin_once(self)
-            if(self.future == None):
-                continue
-            
-            if(self.future.done()):
-                result = self.future.result()
-                self.get_logger().info('Result: ' + str(result.valid))
-                msg = Bool()
-                msg.data = result.valid
-                self.publisher.publish(msg)
-                self.future = None
+    def timer_callback(self):
+        if(self.future == None):
+            return
+        
+        if(self.future.done()):
+            result = self.future.result()
+            self.get_logger().info('Result: ' + str(result.valid))
+            msg = Bool()
+            msg.data = result.valid
+            self.publisher.publish(msg)
+            self.future = None
+        
 
   
 def main():
     rclpy.init()
     client = GeoFenceCheckerClient()
-    client.spin()
+    executor = MultiThreadedExecutor()
+    executor.add_node(client)
+    executor.spin()
     client.destroy_node()
     rclpy.shutdown()
 
